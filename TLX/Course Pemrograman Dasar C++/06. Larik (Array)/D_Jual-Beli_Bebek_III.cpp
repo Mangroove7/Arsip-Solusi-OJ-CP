5eccf1e07367c85724eb9e98fdf33006282b1cf8
Mangrove7 <morgantumcpp@gmail.com>
2025-11-08 19:23:16 +0700

    adding D Problem in 06 segment basic-cpp tlx

---
+++ TLX/Course Pemrograman Dasar C++/06. Larik (Array)/D_Jual-Beli_Bebek_III.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int beli[10] = {13, 100, 0, 4, 31, 0, 178, 23, 1, 13};
+    int jual[10] = {0, 2, 24, 0, 10, 4, 0, 121, 0, 15};
+
+    int jumlah = 0;
+
+    for (int i = 0; i < 10; i++) {
+        jumlah += beli[i];
+        jumlah -= jual[i];
+        cout << jumlah << endl;
+    }
+}
